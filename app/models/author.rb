d3ed005eb0cd0bb741e8d4cf069a07cdcd30d454
Edie Atha <edieatha@gmail.com>
2020-06-25 22:21:06 +0300

    Add validations and associations

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,5 @@ class Author < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
     has_many :posts
+    has_many :comments
 end
