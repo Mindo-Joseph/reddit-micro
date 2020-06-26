65212cfe69de6a1717e50236db775e93b9a720ff
Edgar Athanasius <edieatha@gmail.com>
2020-06-26 11:10:58 +0300

    Update create_comments migration file

--- db/migrate/20200625162742_create_comments.rb
+++ db/migrate/20200625162742_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :author
       t.string :body
       t.references :post
       t.references :author
